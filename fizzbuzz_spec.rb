cf5bd53ad24e8893dbf921afd5adad8f86bcf137
Robert Harężlak <harezlak.robert7@gmail.com>
2016-06-30 17:59:41 +0200

    first expection

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -1,4 +1,12 @@
 require "./fizzbuzz"
 describe FizzBuzz do
+  describe ".div_by_three" do
+    context "divide by 3" do
+      it "should return 0" do
+        expect(FizzBuzz.div_by_three(3)).to eql(0)
+      end
+    end
+  end
+
 
 end
