bc261834eedfaed188c3b26c3c3b4fdcad332a55
Robert Harężlak <harezlak.robert7@gmail.com>
2016-06-30 18:16:55 +0200

    added div by five

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,4 +3,10 @@ class FizzBuzz
     if (input % 3 == 0) then return "Fizz"
     end
   end
+  def self.div_by_five(input)
+    if (input % 5 == 0) then return "Buzz"
+    end
+  end
+
+
 end
--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -2,11 +2,20 @@ require "./fizzbuzz"
 describe FizzBuzz do
   describe ".div_by_three" do
     context "divide by 3" do
-      it "should return 0" do
+      it "should return Buzz" do
         expect(FizzBuzz.div_by_three(3)).to eql("Fizz")
       end
     end
   end
+
+  describe ".div_by_five" do
+    context "divide by 5" do
+      it "should return Buzz" do
+        expect(FizzBuzz.div_by_five(20)).to eql("Buzz")
+      end
+    end
+  end
+
 
 
 end
